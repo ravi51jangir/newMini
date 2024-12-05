d34eaf0252e97c881e54318108b35d7598168363
Ravi Kumar <164409218+ravi51jangir@users.noreply.github.com>
2024-12-05 21:57:47 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,8 @@ import { cookieStorage, createStorage, http } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { arbitrum, sepolia } from "@reown/appkit/networks";
 
-// Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+// Directly set your projectId here
+export const projectId = "537931f27ae9d94a71f7e559a4aff8e8"; // Hardcoded project ID
 
 if (!projectId) {
   throw new Error("Project ID is not defined");
@@ -11,13 +11,13 @@ if (!projectId) {
 
 export const networks = [arbitrum, sepolia];
 
-//Set up the Wagmi Adapter (Config)
+// Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
     storage: cookieStorage,
   }),
   ssr: true,
-  projectId,
+  projectId, // Using the hardcoded projectId here
   networks,
 });
 
